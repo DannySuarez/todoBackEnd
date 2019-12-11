7d6b04f9a7d7d5b6354c395f5625f3b24d969d9b
DannySuarez <suarezd10@gmail.com>
2019-12-11 10:38:07 -0800

    Test post route

--- test/todos.test.js
+++ test/todos.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Todo = require('../lib/models/Todo');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -16,5 +17,22 @@ describe('app routes', () => {
 
   afterAll(() => {
     return mongoose.connection.close();
+  });
+
+  it('can create a todo', () => {
+
+    return request(app)
+      .post('/api/v1/todos')
+      .send({
+        title: 'pass this test'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          __v: 0,
+          title: 'pass this test'
+        });
+      });
+
   });
 });
