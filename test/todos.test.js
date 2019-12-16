6afbf425fa144fbc04b4edd475d4bfa097103bfb
DannySuarez <suarezd10@gmail.com>
2019-12-15 17:40:58 -0800

    refactor todos test to include creation of list first

--- test/todos.test.js
+++ test/todos.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const List = require('../lib/models/List');
 const Todo = require('../lib/models/Todo');
 
 describe('todos route', () => {
@@ -15,30 +16,34 @@ describe('todos route', () => {
     return mongoose.connection.dropDatabase();
   });
 
+  let homelist = null;
   let todo = null;
   let todo2 = null;
-  let todo3 = null;
+  let  todo3 = null;
   beforeEach(async() => {
-    todo = JSON.parse(JSON.stringify(await Todo.create({ title: 'clean dishes' })));
-    todo2 = JSON.parse(JSON.stringify(await Todo.create({ title: 'take out trash' })));
-    todo3 = JSON.parse(JSON.stringify(await Todo.create({ title: 'take a break', completed: true })));
+    homelist =  JSON.parse(JSON.stringify(await List.create({ title: 'home list' })));
+    todo = JSON.parse(JSON.stringify(await Todo.create({ list: homelist._id, title: 'clean dishes' })));
+    todo2 = JSON.parse(JSON.stringify(await Todo.create({ list: homelist._id, title: 'take out trash' })));
+    todo3 = JSON.parse(JSON.stringify(await Todo.create({ list: homelist._id, title: 'take a break', completed: true })));
   });
 
   afterAll(() => {
     return mongoose.connection.close();
   });
 
-  it('can create a todo', () => {
+  it('can create a todo in a list', () => {
     return request(app)
       .post('/api/v1/todos')
       .send({
-        title: 'pass this test'
+        list: homelist._id,
+        title: todo.title
       })
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
           __v: 0,
-          title: 'pass this test'
+          title: 'clean dishes',
+          list: homelist._id.toString()
         });
       });
   });
@@ -50,6 +55,7 @@ describe('todos route', () => {
         expect(res.body).toHaveLength(3);
         expect(res.body).toContainEqual({
           _id: todo._id.toString(),
+          list: homelist._id.toString(),
           title: 'clean dishes',
           __v: 0
         });
@@ -63,6 +69,7 @@ describe('todos route', () => {
       .then(res => {
         expect(res.body).toEqual({
           _id: todo._id.toString(),
+          list: homelist._id.toString(),
           title: 'clean dishes',
           completed: true,
           __v: 0
@@ -78,6 +85,7 @@ describe('todos route', () => {
         expect(res.body).toEqual({
           _id: todo3._id.toString(),
           title: 'take a break',
+          list: homelist._id.toString(),
           completed: false,
           __v: 0
         });
@@ -91,6 +99,7 @@ describe('todos route', () => {
         expect(res.body).toEqual({
           _id: todo2._id.toString(),
           title: 'take out trash',
+          list: homelist._id.toString(),
           __v: 0
         });
       });
